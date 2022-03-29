46e2f40fe5df4f4892a4f54a45d33e229fc2a222
Joshmclemore <josh_mclemore@yahoo.com>
2022-03-29 08:17:25 -0500

    My routes are in place. initial push

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
+import Login from './mocks/components/login';
+import FriendsList from './mocks/components/friendslist';
+import AddFriends from './mocks/components/addfriends';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/">
+          <Login/>
+        </Route>
+        <Route exact path="/login">
+          <Redirect to="/"/>
+        </Route>
+        <Route exact path="/friends">
+          <FriendsList/>
+        </Route>
+        <Route exact path="/friends/add">
+          <AddFriends/>
+        </Route>
+      </div> 
+    </Router>
+
   );
 }
 
